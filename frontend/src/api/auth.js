e29e86ea77fa772d9a961435cf1fe37956ba09ef
summersuhas <sama.suhas13@gmail.com>
2026-05-24 13:11:11 +0530

    fix api requests

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/auth',
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const signupUser = (data) =>

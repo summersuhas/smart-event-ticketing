d384515a038cfa76f4b04bba0f412b61b9d2e316
summersuhas <sama.suhas13@gmail.com>
2026-05-25 15:14:36 +0530

    Add booking UX improvements and event filtering

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,18 +2,27 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type":
+      "application/json",
   },
 });
 
 // Attach JWT token automatically
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem(
+        "token"
+      );
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
   }
+);
 
-  return config;
-});
+export default axiosInstance;
